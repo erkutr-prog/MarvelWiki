d3de475d3b62572b0cf6a590451d1468f64f2abf
erkuturkyilmaz <erkuturkyilmaz@gmail.com.tr>
2023-04-21 16:27:37 +0300

    Added env file for secrets

--- src/utils/Requests.ts
+++ src/utils/Requests.ts
@@ -1,8 +1,10 @@
+import React from 'react';
 import axios from 'axios';
 import md5 from 'md5';
+import { PUBLIC_KEY, PRIVATE_KEY } from '@env'
 
-const publickey = '';
-const privateKey = '';
+const publickey = PUBLIC_KEY;
+const privateKey = PRIVATE_KEY;
 
 const ts = new Date().getTime();
 
